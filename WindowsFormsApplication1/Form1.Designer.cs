10eb2f54052a3730a49da382170fe5c7580c8e04
Nelson Cardenas <nelsonjc.01@gmail.com>
2016-09-19 23:20:34 -0600

    Proyecto completo

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 12);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(481, 374);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.Text = "";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(215, 395);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 1;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 430);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.txtBox);
+            this.Name = "Form1";
+            this.Text = "Proyecto de Lenguajes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.Button btnEjecutar;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LuaInterface;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Lua lua = new Lua();
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Form1 form = new Form1();
+                lua.RegisterFunction("cout", form, form.GetType().GetMethod("cout"));
+                lua.RegisterFunction("agregar", form, form.GetType().GetMethod("agregar"));
+                lua.DoString(txtBox.Text);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void cout (string cadena){
+            MessageBox.Show(cadena);
+        }
+
+        public string agregar(string nombre, string edad) {
+            int numvalue;
+            string rtn = "";
+            try
+            {
+                if(nombre.Length <= 0)
+                {
+                    rtn = "Error. El campo nombre no fue llenado";
+                    throw new Exception();
+                }
+                if (edad.Length <= 0)
+                {
+                    rtn = "Error. El campo edad no fue llenado";
+                    throw new Exception();
+                }
+                rtn = "Error. Ingreso letras u otras cosas en ves de numeros en edad";
+                numvalue = Int32.Parse(edad);
+                rtn = nombre + " tiene " + edad + " años";
+                return rtn;
+            }
+            catch
+            {
+                return rtn;
+            }
+        }
+    }
+}
